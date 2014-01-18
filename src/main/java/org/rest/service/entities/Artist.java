76a49eb0550cc449289f17bda5631f0a6ca85bb3
arthurmagne <arthur.magne@hotmail.fr>
2014-01-18 13:39:38 +0100

    ajout d'infos dans la class artist

--- src/main/java/org/rest/service/entities/Artist.java
+++ src/main/java/org/rest/service/entities/Artist.java
@@ -10,6 +10,9 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.persistence.TemporalType;
+import javax.persistence.Temporal;
+import java.util.Date;
 
 @Entity
 @XmlRootElement
@@ -18,6 +21,12 @@ public class Artist {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
+	private String city;
+	private String nationality;
+	private String photo;
+	private String workType;
+	@Temporal(TemporalType.DATE)
+	private Date birthDate;
 	@ManyToMany(mappedBy="artists")
 	private Set<Artwork> artworks;
 	
@@ -49,6 +58,21 @@ public class Artist {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
 
 	@XmlElementWrapper
 	public Set<Artwork> getArtworks() {
@@ -59,5 +83,22 @@ public class Artist {
 		this.artworks = artworks;
 	}
 	
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	
+	public String getWorkType() {
+		return workType;
+	}
+
+	public void setWorkType(String workType) {
+		this.workType = workType;
+	}
+
+
 	
 }
